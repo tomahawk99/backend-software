b6b06dcdad90b880b67cfd90bda9af9e998f2626
Pablo Álvarez <palvarezs169@gmail.com>
2023-05-31 20:27:09 -0400

    add koa-router

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const Koa = require('koa');
 const { koaBody } = require('koa-body');
 const KoaLogger = require('koa-logger');
+const router = require('./routes');
 
 const app = new Koa();
 
 app.use(KoaLogger());
 app.use(koaBody());
+app.use(router.routes());
 
 app.use(async (ctx, next) => {
   ctx.body = 'Hello World!';
@@ -14,3 +16,4 @@ app.use(async (ctx, next) => {
 app.listen(3000, () => {
   console.log('server is running at http://localhost:3000');
 });
+
---
+++ src/routes/users.js
@@ -0,0 +1,3 @@
+
+// TO DO:
+// create users CRUD
