2517a520ee015eeaf4b767622dc3fd1e1146ff5c
Pablo Álvarez <palvarezs169@gmail.com>
2023-06-05 15:31:53 -0400

    Users and Enclousures CRUDs done

--- src/routes/enclousures.js
+++ src/routes/enclousures.js
@@ -1,24 +1,25 @@
 const Router = require('koa-router');
 const router = new Router();
-const { Enclousures } = require('../models');
+const { Enclousures, Users } = require('../models');
 
 
 
 
 // Create an enclousure
-// FALTA COMPROBAR QUE FUNCIONE, en vola necesito obtener el ownerId
-router.post('/enclousures', async (ctx) => {
+router.post('/enclousures', '/create', async (ctx) => {
   try {
-    const { name, address, district, phoneNumber, socialMedia, email } = ctx.request.body;
     // const ownerId = ctx.state.user.id;
+    const owner = await Users.findByPk(1); // hardcoded for now
+    console.log(owner);
+    ownerId = owner.id;
     const enclousure = await Enclousures.create({
-      name,
-      // ownerId,
-      address,
-      district,
-      phoneNumber,
-      socialMedia,
-      email
+      name: ctx.request.body.name,
+      ownerId: ownerId,
+      address: ctx.request.body.address,
+      district: ctx.request.body.district,
+      phoneNumber:  ctx.request.body.phoneNumber,
+      socialMedia: ctx.request.body.socialMedia,
+      email: ctx.request.body.email
     });
     console.log(enclousure);
     ctx.body = enclousure;
@@ -30,7 +31,7 @@ router.post('/enclousures', async (ctx) => {
 
 
 // Get all Enclousures
-router.get('enclousures', '/', async (ctx) => {
+router.get('/enclousures', '/', async (ctx) => {
   try {
     const enclousures = await Enclousures.findAll();
     console.log(enclousures)
@@ -58,6 +59,54 @@ router.get('/enclousures', '/:id', async (ctx) => {
     ctx.body = { error: 'Failed to retrieve enclousure' };
   }
 });
+
+
+// Update an enclousure
+router.put('/enclousures', '/:id/update',  async (ctx) => {
+  try {
+    const enclousure = await Enclousures.findByPk(ctx.params.id);
+    console.log(enclousure);
+    if (!enclousure) {
+      ctx.status = 404;
+      ctx.body = { error: 'Enclousure not found' };
+    } else {
+      const { name, address, district, phoneNumber, socialMedia, email } = ctx.request.body;
+      await enclousure.update({
+        name,
+        address,
+        district,
+        phoneNumber,
+        socialMedia,
+        email
+      });
+      ctx.body = enclousure;
+    }
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = { error: 'Failed to update enclousure' };
+  }
+});
+
+
+
+// Delete an enclousure
+router.delete('/enclousures', '/:id/delete', async (ctx) => {
+  try {
+    const enclousure = await Enclousures.findByPk(ctx.params.id);
+    console.log(enclousure);
+    if (!enclousure) {
+      ctx.status = 404;
+      ctx.body = { error: 'Enclousure not found' };
+    } else {
+      await enclousure.destroy();
+      ctx.body = { message: 'Enclousure deleted successfully' };
+    }
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = { error: 'Failed to delete enclousure' };
+  }
+});
+
 
 
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -10,8 +10,27 @@ const {Users} = require('../models');
 
 
 
+// Create a user
+router.post('/users', '/create', async (ctx) => {
+  try {
+    const user = await Users.create({
+      name: ctx.request.body.name,
+      lastName: ctx.request.body.lastName,
+      password: ctx.request.body.password,
+      email: ctx.request.body.email,
+      type: ctx.request.body.type
+    });
+    console.log(user);
+    ctx.body = user;
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = { error: 'Failed to create user' };
+  }
+});
+
+
 // Get all users
-router.get('users', '/', async (ctx) => {
+router.get('/users', '/', async (ctx) => {
   try {
     const users = await Users.findAll();
     console.log(users)
@@ -39,6 +58,50 @@ router.get('/users', '/:id', async (ctx) => {
   }
 });
 
+
+// Update a user
+router.put('/users', '/:id/update',  async (ctx) => {
+  try {
+    const user = await Users.findByPk(ctx.params.id);
+    console.log(user);
+    if (!user) {
+      ctx.status = 404;
+      ctx.body = { error: 'User not found' };
+    } else {
+      const { name, lastName, password, email, type } = ctx.request.body;
+      await user.update({
+        name,
+        lastName,
+        password,
+        email,
+        type
+      });
+      ctx.body = user;
+    }
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = { error: 'Failed to update user' };
+  }
+});
+
+
+// Delete a user
+router.delete('/users', '/:id/delete', async (ctx) => {
+  try {
+    const user = await Users.findByPk(ctx.params.id);
+    console.log(user);
+    if (!user) {
+      ctx.status = 404;
+      ctx.body = { error: 'User not found' };
+    } else {
+      await user.destroy();
+      ctx.body = { message: 'User deleted successfully' };
+    }
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = { error: 'Failed to delete User' };
+  }
+});
 
 
 
