f8dd86763a7d27ea534dd3b02689ac8843e87c33
tconcha <tconcha@uc.cl>
2023-06-11 15:43:46 -0400

    app host try

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.use(async (ctx, next) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log('server is running at http://localhost:3000');
 });
 
