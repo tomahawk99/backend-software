6c076ed1fb3228eaf2ede18201110757f309118a
Eliseo Lopez <eliseo.lopez@uc.cl>
2023-06-12 18:30:07 -0400

    app

--- src/app.js
+++ src/app.js
@@ -18,8 +18,9 @@ app.use(async (ctx, next) => {
   ctx.body = 'Hello World!';
 });
 
-app.listen(3000, () => {
-  console.log('server is running at http://localhost:3000');
-});
+// app.listen(3000, () => {
+//   console.log('server is running at http://localhost:3000');
+// });
 
-module.exports = app;
+const server = app.listen(3000);
+module.exports = server;
